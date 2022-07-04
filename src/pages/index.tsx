5ec2df4a6e0c916765c05b2e0e8f22ed5adb686f
Bishram <m.bishram.yashir@gmail.com>
2022-07-04 10:43:48 +0800

    Update landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,14 +5,15 @@ import { NavCard } from "@/components/ui/NavCard";
 const Home: NextPage = () => {
   return (
     <MainLayout title="Dashboard" noHomeButton>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 items-center">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 items-center max-w-screen-md mx-auto">
         <NavCard
           href="/xss"
           header="XSS Scripts"
           description={(Hovered) => (
             <>
               List of <Hovered>scripts</Hovered> used to test Reflected and
-              Stored <Hovered>Cross-Site Scripting</Hovered> Attacks.
+              Stored <Hovered>Cross-Site Scripting</Hovered> Attacks and it's{" "}
+              <Hovered>result</Hovered>.
             </>
           )}
         />
@@ -24,16 +25,6 @@ const Home: NextPage = () => {
               <Hovered>Controlled page</Hovered> disguised as a web that shows a
               picture of cats. It is used to test{" "}
               <Hovered>Cross-Site Request Forgery</Hovered> Attacks.
-            </>
-          )}
-        />
-        <NavCard
-          href="/results"
-          header="Results"
-          description={(Hovered) => (
-            <>
-              Show all <Hovered>Payloads</Hovered> received from{" "}
-              <Hovered>XSS and CSRF</Hovered> Attacks.
             </>
           )}
         />
