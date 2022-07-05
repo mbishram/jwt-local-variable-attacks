55a545376500fa9b1e8fb5ac859af91943bd643f
Bishram <m.bishram.yashir@gmail.com>
2022-07-05 21:18:33 +0800

    Add syntax highlighter component

--- src/pages/xss.tsx
+++ src/pages/xss.tsx
@@ -1,10 +1,18 @@
 import { NextPage } from "next";
 import { MainLayout } from "@/components/layouts/MainLayout";
+import { Typography } from "@/components/ui/Typography";
+import { StyledSyntaxHighlighter } from "@/components/ui/StyledSyntaxHighlighter";
 
 const XSS: NextPage = () => {
+  const xssScript = `(num) => num + 1
+  // this comment is here to demonstrate an extremely long line length, well beyond what you should probably allow in your own code, though sometimes you'll be highlighting code you can't refactor, which is unfortunate but should be handled gracefully `;
+
   return (
     <MainLayout title="XSS">
-      <div>XSS</div>
+      <Typography variant="h2" className="mb-4">
+        XSS Script
+      </Typography>
+      <StyledSyntaxHighlighter>{xssScript}</StyledSyntaxHighlighter>
     </MainLayout>
   );
 };
